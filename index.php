adc8f34f2221031e9680813f3b9d151ac488e4e5
hailm <hai.lemanh.1310@gmail.com>
2023-02-15 10:39:23 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
-<?php $data["title"] = "Home"; ?>
-<?php $bodyClass = '' ?>
+<?php $data["title"] = "trang chủ"; ?>
 <?php require "template-parts/layouts/header.php"; ?>
-
+<div class="uk-background-norepeat uk-background-center-center uk-background-cover uk-section-small uk-flex uk-flex-column uk-text-center" data-src="images/trangchu/bg1.jpg" uk-img="" uk-height-viewport="">
+    <div class="uk-flex-auto">
+        <div class="uk-container">
+            <div class="uk-flex-center uk-grid" uk-grid>
+                <div class="uk-width-1-3@m uk-width-2-3">
+                    <img style="" src="images/trangchu/trangchu_txt.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-container">
+            <div class="uk-flex-center uk-grid" uk-grid>
+                <div class="uk-width-1-2 uk-width-1-4@s">
+                    <div><a href=""><img src="images/trangchu/btn-batdau.png" alt=""></a></div>
+                    <div><a href=""><img src="images/trangchu/btn-dangnhap.png" alt=""></a></div>
+                    <div class="uk-child-width-1-3 uk-flex-center uk-margin-top uk-grid" uk-grid>
+                        <div>
+                            <a href=""><img src="images/trangchu/fb-logo.png" alt=""></a>
+                        </div>
+                        <div>
+                            <a href=""><img src="images/trangchu/gmail-logo.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
